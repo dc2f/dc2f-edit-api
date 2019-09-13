d440c477046e5eba8971570c0d5831538efde056
Herbert Poul <herbert@poul.at>
2019-09-14 00:29:08 +0200

    automatically reconnect when websocket disconnects (probably because of recompile/reload)

--- src/main/kotlin/com/dc2f/api/edit/ApiHandler.kt
+++ src/main/kotlin/com/dc2f/api/edit/ApiHandler.kt
@@ -59,20 +59,65 @@ class ApiHandler(val deps: Deps<*>) {
 // language="ECMAScript 6"
 private val jsInject = """
 const host = window.location.host;
-const socket = new WebSocket('ws://'+host+'/api/ws');
+let gotReloadSignal = false;
+let isReconnecting = false;
 
-// Connection opened
-socket.addEventListener('open', function (event) {
-    socket.send('Hello Server!');
-    console.log('we are connected.', event);
-});
-
-// Listen for messages
-socket.addEventListener('message', function (event) {
-    console.log('Message from server ', event.data);
-    if (event.data === 'reload') {
-        window.location.reload();
+function connectToWebSocket() {
+    let handledDisconnect = false;
+    const socket = new WebSocket('ws://'+host+'/api/ws');
+    
+    // Connection opened
+    socket.addEventListener('open', function (event) {
+        socket.send('Hello Server!');
+        console.log('we are connected.', event);
+        if (isReconnecting) {
+            gotReloadSignal = true;
+            window.location.reload();
+        }
+    });
+    
+    // Listen for messages
+    socket.addEventListener('message', function (event) {
+        console.log('Message from server ', event.data);
+        if (event.data === 'reload') {
+            gotReloadSignal = true;
+            window.location.reload();
+        }
+    });
+    
+    function maybeRetry() {
+        console.log('maybeRetry: gotReloadSignal:' + gotReloadSignal + ' didRetry:" + didRetry');
+        if (!gotReloadSignal && !handledDisconnect) {
+            handledDisconnect = true;
+            if (!isReconnecting) {
+                isReconnecting = true;
+                const body = document.querySelector('body');
+                const div = document.createElement('div');
+                div.classList.add('loading');
+                div.style.position = 'fixed';// = "position: fixed; top: 0; left: 0; right: 0; bottom:0; background-color: #000000; opacity: 0.5;";
+                div.style.top = div.style.right = div.style.bottom = div.style.left = 0;
+                div.style.backgroundColor = '#000000';
+                div.style.opacity = '0.5';
+                div.style.zIndex = '10000';
+                // noinspection JSCheckFunctionSignatures
+                body.appendChild(div);
+            }
+            setTimeout(function() {
+                connectToWebSocket();
+            }, 300);
+        }
     }
-});
+    
+    socket.addEventListener('error', function (event) {
+        console.log('Error from websocket. ', event);
+        maybeRetry();
+    });
+    
+    socket.addEventListener('close', function (event) {
+        console.log('Closed websocket.', event);
+        maybeRetry();
+    })
+}
+connectToWebSocket();
 
 """.trimIndent()
